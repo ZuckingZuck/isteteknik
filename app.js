82ea175a1db4902d11ba971952de2ca830a5533b
ZuckingZuck <68840782+ZuckingZuck@users.noreply.github.com>
2021-04-01 11:56:48 +0300

    first

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const cookieParser = require('cookie-parser');
 
 const port = process.env.WEB_CONCURRENCY || 8080;
 
-const user = require('./models/user');
+const User = require('./models/User');
 
 
 const ConnectionString = 'mongodb+srv://OTY:mahir1453@cluster0.y65b2.mongodb.net/filmbotu?retryWrites=true&w=majority';
